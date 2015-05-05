98d786897a54cdf99bec05356be9f41af91cc1f5
John Winstead <awatemonosan@gmail.com>
2015-05-05 08:47:48 -0700

    Make touches work on android builds

---
+++ Assets/Scripts/DebugText.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+function Start () {
+
+}
+var AvarageTouchPosition = function(){
+  var pos = new Vector3();
+  for (var touch : Touch in Input.touches) {
+    pos.x+=touch.position.x;
+    pos.y+=touch.position.y;
+  }
+  
+  pos = pos / Input.touchCount;
+  return pos;
+};
+
+function Update () {
+	
+  var position=Input.mousePosition;
+  if(Input.touchCount>0){
+	position = AvarageTouchPosition();
+  }
+  GetComponent(Text).text="Touches: "+Input.touchCount+"\nPosition: "+position;
+}
